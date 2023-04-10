fc62ffdde1f09189c9e0b46bf54d229d171548c7
RNtaate <royhyde14@gmail.com>
2023-04-10 13:51:13 +0300

    Add helper functions to the NewItemForm to capture data
    Update the design of the component

--- src/components/NewItemForm.jsx
+++ src/components/NewItemForm.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewItemForm = () => {
+
+  const [newTodo, setNewTodo] = useState({title: "", description: ""})
+
+  const handleSettingTitle = (e) => {
+    setNewTodo(currentTodo => {
+      return(
+        {...currentTodo, title: e.target.value}
+      )
+    })
+  }
+
+  const handleSettingDescription = (e) => {
+    setNewTodo(currentTodo => {
+      return(
+        {...currentTodo, description: e.target.value}
+      )
+    })
+  }
+
+  const handleSubmitingForm = (e) => {
+    e.preventDefault();
+    const {title, description} = newTodo;
+  }
+
   return (
-    <form className="item-form" >
+    <form className="item-form" onSubmit={handleSubmitingForm} >
       <h4>New Todo</h4>
       <label htmlFor='item-title' >Tiltle</label>
-      <input type="text" id="item-title" />
-      <lable>Description</lable>
-      <textarea></textarea>
+      <input type="text" id="item-title" value={newTodo.title} onChange={handleSettingTitle} autoComplete="off" />
+      <label>Description</label>
+      <textarea value={newTodo.description} onChange={handleSettingDescription} ></textarea>
+      <button type='submit' >Add</button>
     </form>
   )
 }
