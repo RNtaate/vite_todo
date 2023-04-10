82d2cfef796ab802e359407bc8cee0ac44266e90
RNtaate <royhyde14@gmail.com>
2023-04-10 14:27:44 +0300

    Add the todos data set using useState
    Update the design by adding the list of items based on entered data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
+import { useState, useEffect } from 'react'
+
 import './App.css'
 import NewItemForm from './components/NewItemForm'
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    console.log(todos)
+  }, [todos])
+
+  const handleAddingTodo = (newTodo) => {
+    setTodos(currentTodos => {
+      return (
+        [...currentTodos, newTodo]
+      )
+    })
+  }
+
   return (
     <div className="App">
       <div className='upper-div' >
-        <NewItemForm />
+        <NewItemForm addTodo={handleAddingTodo} />
       </div>
 
       <div className='lower-div'>
-        <aside className="items-list-aside" ></aside>
+        <aside className="items-list-aside" >
+          <h4>Todos</h4>
+          {todos.length === 0 && <h6>No Todos Created</h6>}
+          <ul>
+            {todos.map( item => {
+              return(
+              <li key={item.id}>
+                <input type="checkbox" id={item.id} />
+                <label htmlFor={item.id} >{item.title}</label>
+                <button>Delete</button>
+              </li>            
+              )
+            })}
+          </ul>
+        </aside>
         <div className="details-container-div" ></div>
       </div>
     </div>
