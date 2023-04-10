01ffb75a251747027c549d3008f6d6b8b4d4297d
RNtaate <royhyde14@gmail.com>
2023-04-10 16:55:08 +0300

    Add delete button to the Todo Details component and implement its functionality

--- src/components/TodoDetails.jsx
+++ src/components/TodoDetails.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { TodosContext } from '../context/TodosContextProvider'
 
 export default function TodoDetails({todo}) {
+
+  const todosContext = useContext(TodosContext);
+  const { handleDeletingTodo } = todosContext;
+
   return (
     <div>
       <h1>{todo.title}</h1>
       <p>{todo.description}</p>
-      <span><b>STATUS: </b>Not Completed</span>
+      <span><b>STATUS: </b>{todo.completed ? "Completed" : "Not completed"}</span>
+      <button onClick={() => handleDeletingTodo(todo)} >DELETE</button>
     </div>
   )
 }
