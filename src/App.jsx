87cd4add4d25a7f4192d3c9db586374caeec8003
RNtaate <royhyde14@gmail.com>
2023-04-17 13:34:11 +0300

    Update the design of App container with asociated styling adjustments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,47 @@
 import { useContext, useState } from 'react'
 
 import './App.css'
-import NewItemForm from './components/NewItemForm';
-import TodosList from './components/TodosList';
-import { TodosContext } from './context/TodosContextProvider';
-import TodoDetails from './components/TodoDetails';
+import NewItemForm from './components/NewItemForm'
+import TodosList from './components/TodosList'
+import { TodosContext } from './context/TodosContextProvider'
+import TodoDetails from './components/TodoDetails'
 
 function App() {
-
-  const [showForm, setShowForm] = useState(false);
-  const todosContext = useContext(TodosContext);
-  const { todos, selectedTodo } = todosContext;
+  const [showForm, setShowForm] = useState(false)
+  const todosContext = useContext(TodosContext)
+  const { todos, selectedTodo } = todosContext
 
   return (
     <div className="App">
-      <div className='upper-div'
-        onClick={() => (setShowForm(false))}
-        style={
-          {
-            zIndex: showForm ? 10 : -1,
-            opacity: showForm ? 1 : 0,
-          }
-        }
+      <div
+        className="upper-div"
+        onClick={() => setShowForm(false)}
+        style={{
+          zIndex: showForm ? 10 : -1,
+          opacity: showForm ? 1 : 0,
+        }}
       >
-        <NewItemForm setShowForm={setShowForm}/>
+        <NewItemForm setShowForm={setShowForm} />
       </div>
 
-      <div className='lower-div'>
-        <aside className="items-list-aside" >
+      <div className="lower-div">
+        <aside className="items-list-aside">
           <h1>Todos</h1>
-          {Object.keys(todos).length === 0 && <h6>No todos yet</h6>}
+          {Object.keys(todos).length === 0 && <h6>No todos created</h6>}
           <TodosList />
-          <button className="add-todo-btn btn" onClick={() => setShowForm(true)} >Add Todo</button>
+          <button
+            className="add-todo-btn btn"
+            onClick={() => setShowForm(true)}
+          >
+            Add Todo
+          </button>
         </aside>
-        <div className="details-container-div" >
-          {selectedTodo ? <TodoDetails todo={selectedTodo}/> : <p>No Todo Selected</p>}
+        <div className="details-container-div">
+          {selectedTodo ? (
+            <TodoDetails todo={selectedTodo} />
+          ) : (
+            <p className="no-selection-p">No Todo Selected</p>
+          )}
         </div>
       </div>
     </div>
